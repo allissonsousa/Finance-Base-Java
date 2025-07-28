7da8c5b922969d75b56a481a7221e893e1813bc3
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2025-07-28 11:12:21 -0300

    Initialize Spring Boot project for negocio app
    Add initial Spring Boot application structure with Maven wrapper, configuration files, and basic test. Includes pom.xml with dependencies for web, JPA, SQL Server, and Tomcat, as well as main and test application classes.

---
+++ negocio/src/main/java/negociosbase/devrun/negocio/ServletInitializer.java
@@ -0,0 +1,13 @@
+package negociosbase.devrun.negocio;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(NegocioApplication.class);
+	}
+
+}
