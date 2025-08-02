c627c491309673e5daa153086fc2f0049d797abf
Allison de Paulo Sousa Avila <111160403+allissonsousa@users.noreply.github.com>
2025-08-02 10:16:37 -0300

    Remove Tomcat starter dependency from pom.xml
    Eliminated the spring-boot-starter-tomcat dependency with 'provided' scope from the project pom.xml. No functional changes were made to application source files.

--- negocio/src/main/java/negociosbase/devrun/negocio/NegocioApplication.java
+++ negocio/src/main/java/negociosbase/devrun/negocio/NegocioApplication.java
@@ -3,11 +3,11 @@ package negociosbase.devrun.negocio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class NegocioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NegocioApplication.class, args);
 	}
+
 
 }
